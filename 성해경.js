66158d2d62959cb18bd62b94ce0d06829f2b9dd4
HaekyungSeong <haekyungs95@gmail.com>
2023-04-24 23:32:03 +0900

    태그생성함수까진 생성, 배열은.. 어디로.. 갈까..

--- 성해경.js
+++ 성해경.js
@@ -1,15 +1,15 @@
 let basicData = {
-  header : {
+  header: {
     id: "header",
     type: "div",
     children: null,
   },
-  main : {
+  main: {
     id: "main",
     type: "div",
     children: null,
   },
-  footer : {
+  footer: {
     id: "footer",
     type: "div",
     children: null,
@@ -18,10 +18,34 @@ let basicData = {
 
 let elementData = ["게시판", "수업과제", "사용자", "성적", "로그아웃"];
 
-function exampleOne(object, array){
-  let value;
-
-  return value;
+function exampleOne(object, array) {
+  // let value;
+  Object.keys(object).forEach((e) => {
+    if (object[e].children === null) {
+      // console.log(object[e])
+      // console.dir(element)
+      const element = document.createElement(e);
+      document.body.appendChild(element);
+      // Object.assign(element, object[e]);
+      // assign 은 오류가 발생한다. 그건 저 class 에 셋팅 문제.
+      for(let key in object[e]){
+        element.setAttribute(key,object[e].key)
+      }
+    }
+  })
+  // return value;
 }
 
-console.log(exampleOne(basicData,elementData));
+// document.body.innerHTML=exampleOne(basicData,elementData);
+// exampleOne(basicData, elementData)
+
+
+console.log(exampleOne(basicData, elementData));
+
+
+// 몽땅 문자열로 만든 거
+// let attri = ""
+// for(let key in object[e]){
+//   attri += `${key}='${object[e][key]}' `
+// }
+// return value += `<${e} ${attri}></${e}> `
