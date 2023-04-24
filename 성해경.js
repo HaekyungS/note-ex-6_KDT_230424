5f213b58a3e6626061867b8822405f3b5cb9d9f3
HaekyungSeong <haekyungs95@gmail.com>
2023-04-24 19:45:34 +0900

    exam6 기본틀 작성

---
+++ 성해경.js
@@ -0,0 +1,27 @@
+let basicData = {
+  header : {
+    id: "header",
+    type: "div",
+    children: null,
+  },
+  main : {
+    id: "main",
+    type: "div",
+    children: null,
+  },
+  footer : {
+    id: "footer",
+    type: "div",
+    children: null,
+  }
+}
+
+let elementData = ["게시판", "수업과제", "사용자", "성적", "로그아웃"];
+
+function exampleOne(object, array){
+  let value;
+
+  return value;
+}
+
+console.log(exampleOne(basicData,elementData));
